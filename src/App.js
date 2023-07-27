c78c380df3a015839cae6b5ed11838a74a2a7947
Borys Osaulenko <Borys.Osaulenko@MBP-Vladimir.lan>
2023-07-27 15:57:21 +0200

    dummy experiment

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
 import React from 'react';
-import { Route, Routes, BrowserRouter } from 'react-router-dom'
-import Articles from './containers/Articles';
-import User from './components/User';
-import Home from './containers/Home';
+// import { Route, Routes, BrowserRouter } from 'react-router-dom'
+// import Articles from './containers/Articles';
+// import User from './components/User';
+// import Home from './containers/Home';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Articles />} />
-        <Route path='/users' element={<User />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path='/' element={<Articles />} />
+    //     <Route path='/users' element={<User />} />
+    //     <Route path='/home' element={<Home />} />
+    //     <Route path='/login' element={<Login />} />
+    //   </Routes>
+    // </BrowserRouter>
+    aa
   );
 }
 
