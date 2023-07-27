825629c8a036a5a6f44ee17be155f25f4a901fd5
Borys Osaulenko <Borys.Osaulenko@MBP-Vladimir.lan>
2023-07-27 16:02:48 +0200

    end of the hotfix

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import './App.css';
 import React from 'react';
-// import { Route, Routes, BrowserRouter } from 'react-router-dom'
-// import Articles from './containers/Articles';
-// import User from './components/User';
-// import Home from './containers/Home';
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import Articles from './containers/Articles';
+import User from './components/User';
+import Home from './containers/Home';
 
 function App() {
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path='/' element={<Articles />} />
-    //     <Route path='/users' element={<User />} />
-    //     <Route path='/home' element={<Home />} />
-    //     <Route path='/login' element={<Login />} />
-    //   </Routes>
-    // </BrowserRouter>
-    <div>aa</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Articles />} />
+        <Route path='/users' element={<User />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
