a8bdf717938e7b7c6bfe0ad48ccb907028652032
Borys Osaulenko <Borys.Osaulenko@MBP-Vladimir.lan>
2023-07-28 14:48:28 +0200

    login done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
+  console.log(process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID);
   return (
     <div className="relative w-full h-screen flex items-center justify-center">
       <video
@@ -14,15 +16,18 @@ const Login = () => {
 
       <GoogleOAuthProvider>
         <GoogleLogin
-          clientId=""
+          clientId={process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID}
           render={(renderProps) => {
             <button
               type="button"
               className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
               onClick={renderProps.onClick}
               disabled={renderProps.disabled}
-            ></button>;
+            >
+              <FcGoogle className="mr-4" /> Sign in with your Google Account
+            </button>;
           }}
+          cookiePolicy="single_host_origin"
           onSuccess={() => {}}
         />
       </GoogleOAuthProvider>
