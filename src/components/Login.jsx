1509f44bd4d3ca921d2b3357d99447723c8ec112
Borys Osaulenko <Borys.Osaulenko@MBP-Vladimir.lan>
2023-07-28 15:16:54 +0200

    fix

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,7 +5,7 @@ import { FcGoogle } from "react-icons/fc";
 const Login = () => {
   console.log(process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID);
   return (
-    <div className="relative w-full h-screen flex items-center justify-center">
+    <div className="relative w-screen h-screen flex items-center justify-center">
       <video
         src="/media/share.mp4"
         className="absolute top-0 bottom-0 left-0 right-0"
@@ -14,9 +14,12 @@ const Login = () => {
         loop
       />
 
-      <GoogleOAuthProvider>
+      <GoogleOAuthProvider
+        clientId={
+          "705664167383-t37t3itqu9t1ceoq90u6ncv9e7ldctcd.apps.googleusercontent.com"
+        }
+      >
         <GoogleLogin
-          clientId={process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID}
           render={(renderProps) => {
             <button
               type="button"
@@ -29,6 +32,9 @@ const Login = () => {
           }}
           cookiePolicy="single_host_origin"
           onSuccess={() => {}}
+          onError={() => {
+            console.log(":(");
+          }}
         />
       </GoogleOAuthProvider>
     </div>
