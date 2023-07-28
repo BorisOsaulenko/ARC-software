1a6b3cb048dfdd1d60c06e964a1eac0e279104d8
Borys Osaulenko <Borys.Osaulenko@MBP-Vladimir.lan>
2023-07-28 14:18:40 +0200

    redone for master branch as default

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 
 const Login = () => {
   return (
-    <div className=' text-red-800'>Login</div>
-  )
-}
+    <div className="relative w-full h-screen flex items-center justify-center">
+      <video
+        src="/media/share.mp4"
+        className="absolute top-0 bottom-0 left-0 right-0"
+        autoPlay
+        muted
+        loop
+      />
 
-export default Login
+      <GoogleOAuthProvider>
+        <GoogleLogin
+          clientId=""
+          render={(renderProps) => {
+            <button
+              type="button"
+              className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+              onClick={renderProps.onClick}
+              disabled={renderProps.disabled}
+            ></button>;
+          }}
+        />
+      </GoogleOAuthProvider>
+    </div>
+  );
+};
+
+export default Login;
